86ea7d81fb5b08da00e2f3f1d68de6da509ff65c
Jorge Lago <jorgelago92@gmail.com>
2022-10-06 14:00:23 -0300

    Adicionando Categoria nova no formulário

--- jogoteca.py
+++ jogoteca.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template, request, redirect
 
 class Jogo:
-    def __init__(self, nome, categoria, console):
+    def __init__(self, nome, categoria, console, multiplayer):
         self.nome = nome
         self.categoria = categoria
         self.console = console
+        self.multiplayer = multiplayer
 
-jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
-jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari', 'Não')
+jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2', 'Não')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2', 'Sim')
 lista = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('lista.html', titulo='Jogos', jogos=lista)
+    return render_template('lista.html', titulo='Jogos que Pretendo Jogar', jogos=lista)
 
 @app.route('/novo')
 def novo():
@@ -26,7 +27,8 @@ def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
-    jogo = Jogo(nome, categoria, console)
+    multiplayer = request.form['multiplayer']
+    jogo = Jogo(nome, categoria, console, multiplayer)
     lista.append(jogo)
     return redirect("/")
 
